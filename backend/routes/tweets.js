e74e42e80c4dd2160248bd5d0088faad60dadf69
Renat Gatin <rgatin@trustarc.com>
2022-11-11 18:10:44 -0800

    HERA-21 Test Dev CICD.

--- backend/routes/tweets.js
+++ backend/routes/tweets.js
@@ -7,7 +7,7 @@ const tweetsRoutes = express.Router();
 
 module.exports = function (DataHelpers) {
   tweetsRoutes.get("/test", function (req, res) {
-    return res.status(200).send("Hello world, from backend! Test Dev.");
+    return res.status(200).send("Hello world, from backend! Test Dev 1.");
   });
 
   tweetsRoutes.get("/", function (req, res) {
