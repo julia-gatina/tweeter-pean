c0e06a120c7f58655ea4489d19eaac12b810ed35
Julia <julia.sarandi@gmail.com>
2022-11-14 17:36:37 -0800

    HERA-24 Create precommit.sh file

---
+++ frontend/precommit.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+git diff-files --quiet
+DIRTY_CODE=$?
+
+if [[ $DIRTY_CODE -ne 0 ]]; then
+  echo "YOU HAVE UNCOMMITTED CODE, PLEASE STASH CHANGES OR ADD FILES"
+  exit 1;
+fi
+
+# npm run lint && npm run test
+npm run lint && npm run format:fix &&  npm run test:ci
+CODE=$?
+
+if [[ $CODE -ne 0 ]]; then
+  echo "---MAKE SURE YOU ADDED ALL YOUR FILES BEFORE COMMITTING---"
+fi
+
+exit $CODE
