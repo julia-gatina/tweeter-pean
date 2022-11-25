d292fa4f77ceb6f33c1f86f530800fc394d8b32e
Renat Gatin <rgatin@trustarc.com>
2022-11-25 13:56:21 -0800

    HERA-32 Set to Env vars DB credentials for Dev, QA and Prod.

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -1,6 +1,23 @@
 module.exports = {
   apps: [
     {
+      /*
+       * This environment is created just to test locally how server
+       * is going to start on remote environments.
+       *
+       * Env variables DB_USERNAME and DB_PASSWORD should be passed
+       * with pm2 command! (saved in a GitHub secrets)
+       */
+      name: 'api-local',
+      script: './index.js',
+      env: {
+        NODE_ENV: 'local',
+        PORT: 8080,
+        DB_PORT: 5430,
+        DB_NAME: 'tweeter'
+      }
+    },
+    {
       name: 'api-develop',
       script: './index.js',
       env: {
