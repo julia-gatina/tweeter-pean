2f0facf1d5dd33ad4dcc90b122ef651ef9d1974c
Julia <julia.sarandi@gmail.com>
2022-12-21 16:45:56 -0800

    HERA-36 Add enum for tweet type to Tweet interface

--- frontend/src/app/shared/components/dashboard/dashboard.model.ts
+++ frontend/src/app/shared/components/dashboard/dashboard.model.ts
@@ -1,8 +1,10 @@
+import { TweetType } from './dashboard.enum';
+
 export interface Tweet {
   name: string;
   username: string;
   avatar: string;
   message: string;
   created_at: Date;
-  type: string;
+  type: TweetType;
 }
