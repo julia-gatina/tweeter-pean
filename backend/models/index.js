717777f6c3e31529d2beced8162622c91f94e3d4
Renat Gatin <rgatin@trustarc.com>
2023-01-08 22:45:51 -0800

    HERA-44 Sequelize use ENV variables for DB setup.

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,19 +1,18 @@
 'use strict';
 
+/* imports */
+const envVar = require('../env-vars');
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+const sequelize = new Sequelize(envVar.DB_NAME, envVar.DB_USERNAME, envVar.DB_PASSWORD, {
+  dialect: 'postgres',
+  host: 'localhost',
+  port: envVar.DB_PORT
+});
 
 fs.readdirSync(__dirname)
   .filter((file) => {
