7ff2c4bf1bcec04bd03fa522c31e5d1bc286bbf9
Julia <julia.sarandi@gmail.com>
2023-01-09 20:58:38 -0800

    HERA-53 Make schemas for PersonResponseDto and PostPersonRequestDto

---
+++ backend/tools/swagger/schemas/person.js
@@ -0,0 +1,43 @@
+module.exports = {
+  components: {
+    schemas: {
+      /**
+       * @openapi
+       * components:
+       *  schemas:
+       *    PersonResponseDto:
+       *      type: object
+       *      properties:
+       *        name:
+       *          type: string
+       *        age:
+       *          type: smallInt
+       *
+       *      example:
+       *        name: 'Jane Doe'
+       *        age: 35
+       */
+      PersonResponseDto: {},
+
+      /**
+       * @openapi
+       * components:
+       *  schemas:
+       *    PostPersonRequestDto:
+       *      type: object
+       *      required:
+       *        - name
+       *      properties:
+       *        name:
+       *          type: string
+       *        age:
+       *            type: smallInt
+       *
+       *      example:
+       *        name: 'John Smith'
+       *        age: 28
+       */
+      PostPersonRequestDto: {}
+    }
+  }
+};
