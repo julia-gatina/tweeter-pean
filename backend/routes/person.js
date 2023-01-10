2ebc67483ad9944b5b358768357ad7c79784b543
Julia <julia.sarandi@gmail.com>
2023-01-09 20:56:10 -0800

    HERA-53 Add person routes swagger docs

--- backend/routes/person.js
+++ backend/routes/person.js
@@ -7,7 +7,24 @@ const personRoutes = express.Router();
 
 module.exports = function () {
   /**
-   * TODO: add Swagger Docs
+   * @openapi
+   * /api/person/all:
+   *  get:
+   *     tags:
+   *     - Persons
+   *     summary: Gets all persons
+   *     description: Return all persons
+   *     responses:
+   *       200:
+   *        description: Success
+   *        content:
+   *          application/json:
+   *            schema:
+   *              type: array
+   *              items:
+   *                $ref: '#/components/schemas/PersonResponseDto'
+   *       500:
+   *        description: Internal server error
    */
   personRoutes.get('/person/all', function (req, res) {
     personService
@@ -21,7 +38,31 @@ module.exports = function () {
   });
 
   /**
-   * TODO: add Swagger Docs
+   * @openapi
+   * /api/person:
+   *  post:
+   *     tags:
+   *     - Persons
+   *     summary: Create new a person
+   *     description: Create new a person
+   *     requestBody:
+   *         description: Create a new person
+   *         content:
+   *           application/json:
+   *             schema:
+   *               $ref: '#/components/schemas/PostPersonRequestDto'
+   *         required: true
+   *     responses:
+   *       200:
+   *        description: Success
+   *        content:
+   *          application/json:
+   *            schema:
+   *              $ref: '#/components/schemas/PersonResponseDto'
+   *       500:
+   *        description: Internal server error
+   *       400:
+   *        description: Invalid request
    */
   personRoutes.post('/person', function (req, res) {
     const person = req.body;
