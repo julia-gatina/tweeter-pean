79c8bd5979daa25972b38731e1bba679c0fe30fb
Julia <julia.sarandi@gmail.com>
2023-01-09 22:35:38 -0800

    HERA-53 Add id properties for tweet and person schemas

--- backend/tools/swagger/schemas/person.js
+++ backend/tools/swagger/schemas/person.js
@@ -8,12 +8,15 @@ module.exports = {
        *    PersonResponseDto:
        *      type: object
        *      properties:
+       *        id:
+       *          type: string
        *        name:
        *          type: string
        *        age:
        *          type: smallInt
        *
        *      example:
+       *        id: '98d9250e-77db-4587-9c12-96302722b161'
        *        name: 'Jane Doe'
        *        age: 35
        */
