cf0a4771d24a34e1e012ff5913bc7d2ad5a5021b
Julia <julia.sarandi@gmail.com>
2023-01-19 22:50:20 -0800

    HERA-55 Change nav menu, add links and logo

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardModule } from './shared/components/dashboard/dashboard.module';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, LoginComponent],
   imports: [BrowserModule, AppRoutingModule, DashboardModule],
   providers: [],
   bootstrap: [AppComponent],
