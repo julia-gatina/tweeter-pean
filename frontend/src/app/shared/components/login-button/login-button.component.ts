2ed30dae8234e741e9ba13f39a0953a09aa33385
Renat Gatin <rgatin@trustarc.com>
2024-02-12 23:51:30 -0800

    HERA-56 Implement Login Auth0 button.

--- frontend/src/app/shared/components/login-button/login-button.component.ts
+++ frontend/src/app/shared/components/login-button/login-button.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '@auth0/auth0-angular';
 
 @Component({
   selector: 'bf-login-button',
   templateUrl: './login-button.component.html',
-  styleUrls: ['./login-button.component.scss']
+  styleUrls: ['./login-button.component.scss'],
 })
 export class LoginButtonComponent implements OnInit {
+  constructor(public authService: AuthService) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  public loginButtonClicked(): void {
+    this.authService.loginWithRedirect();
   }
-
 }
