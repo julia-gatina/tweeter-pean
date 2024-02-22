c24cfc92af7efe0f1f52a8692e8f45f8db0b6b47
Renat Gatin <rgatin@trustarc.com>
2024-02-21 18:19:09 -0800

    HERA-56 Style user profile.

--- frontend/src/app/shared/components/protected/user-profile/user-profile.component.ts
+++ frontend/src/app/shared/components/protected/user-profile/user-profile.component.ts
@@ -1,12 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { map, Observable } from 'rxjs';
+import { AuthService } from '@auth0/auth0-angular';
 
 @Component({
   selector: 'bf-user-profile',
   templateUrl: './user-profile.component.html',
   styleUrls: ['./user-profile.component.scss'],
 })
-export class UserProfileComponent implements OnInit {
-  constructor() {}
+export class UserProfileComponent {
+  public readonly title = 'Decoded ID Token';
 
-  ngOnInit(): void {}
+  public user$: Observable<any>;
+  public code$: Observable<string>;
+
+  constructor(private authService: AuthService) {
+    this.user$ = this.authService.user$;
+    this.code$ = this.user$.pipe(map((user) => JSON.stringify(user, null, 2)));
+  }
 }
